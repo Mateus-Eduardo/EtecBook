44efa501da3a1455caeb0d2ca403aa88cf387e3f
Mateus Eduardo Silvério <115047768+Mateus-Eduardo@users.noreply.github.com>
2023-10-10 22:27:29 -0300

    primero commit

---
+++ EtecBookServer/EtecBookAPI/Models/AppUser.cs
@@ -0,0 +1,35 @@
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EtecBookAPI.Models
+{
+    [Table("User")]
+    public class AppUser
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(60)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(80, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        public string Token { get; set; }
+
+        public string Role { get; set; }
+
+        [Required]
+        [StringLength(300)]
+        public string Email { get; set; }
+        
+        public string ProfilePicture { get; set; }
+    }
+}
